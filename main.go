192a9b8dfd17cca76f735271fe55eb15c34939ac
Barret Rennie <barret@beanbaginc.com>
2018-02-08 14:09:47 -0600

    Reload configuration and restart server when config file changes
    We now monitor the config file path with `fsnotify` to see when it
    changes. When we get any event or receive the SIGHUP signal, we reload
    the configuration and restart the server. This is possible since Golang
    1.8 added the ability to stop a `http.Server` gracefully.
    
    We also gracefully shut down the server upon receipt of a SIGINT and a
    second SIGINT received before graceful shutdown will force-quit the
    server.
    
    Testing Done:
    Ran unit tests.
    
    Tested that the following resulted in configuration being reloaded:
    
     - `touch config.json`
     - editing `config.json`
     - `rm config.json` (results in server crash since there is no
       configuration to reload)
     - `killall -HUP rb-gateway`
    
    In the cases where the server should restart, it did on the correct
    port.
    
    Reviewed at https://reviews.reviewboard.org/r/9568/

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 const (
@@ -45,15 +52,69 @@ func (h *logHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal("Could not watch configuration: ", err)
+	}
+
+	if err = watcher.Add(config); err != nil {
+		log.Fatal("Could not watch configuration: ", err)
+	}
+
 	LoadConfig(config)
 
-	log.Println("Starting rb-gateway server at port", GetPort())
-	log.Println("Quit the server with CONTROL-C.")
+	server := &http.Server{
+		Addr:    ":" + strconv.Itoa(GetPort()),
+		Handler: &logHTTPHandler{Route()},
+	}
 
-	handler := &logHTTPHandler{Route()}
+	hup := make(chan os.Signal, 1)
+	signal.Notify(hup, syscall.SIGHUP)
 
-	err := http.ListenAndServe(":"+strconv.Itoa(GetPort()), handler)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	for {
+		shouldExit := false
+		log.Println("Starting rb-gateway server on port", GetPort())
+		log.Println("Quit the server with CONTROL-C.")
+
+		go func() {
+			if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+				log.Fatal("listenAndServe: ", err)
+			}
+		}()
+
+		select {
+		case <-watcher.Events:
+			log.Println("Detected configuration change, reloading...")
+
+		case watchErr := <-watcher.Errors:
+			log.Fatal("Unexpected error: ", watchErr)
+
+		case <-hup:
+			log.Println("Received SIGHUP, reloading configuration...")
+
+		case <-interrupt:
+			shouldExit = true
+			signal.Reset(os.Interrupt)
+			log.Println("Received SIGINT, shutting down...")
+			log.Println("CONTROL-C again to force quit.")
+		}
+
+		/*
+		 * This allows us to give the server a grace period for finishing
+		 * in-progress requests before it closes all connections.
+		 */
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		server.Shutdown(ctx)
+		log.Println("Server shut down.")
+
+		if shouldExit {
+			os.Exit(0)
+		}
+
+		LoadConfig(config)
+		server.Addr = ":" + strconv.Itoa(GetPort())
 	}
 }
