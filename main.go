b8fc7e4855dddcbd94f46676baa5ad930ba5ffcb
Barret Rennie <barret@beanbaginc.com>
2018-02-09 16:18:21 -0600

    Cleanup HTTP logging
    The HTTP logging utilities have been moved to their own module to clean
    up main.go, which is getting crowded. The old `logHttpHandler` has been
    replaced with the `logRequest` decorator and the logging format has been
    updated to use the Common Logging Format which is logged by most HTTP
    daemons.
    
    A new convenience method has been added that returns an http server
    configured with the RB Gateway routes.
    
    Testing Done:
    Compiled.
    Ran unit tests.
    
    Reviewed at https://reviews.reviewboard.org/r/9605/

--- main.go
+++ main.go
@@ -17,40 +17,6 @@ const (
 	config = "config.json"
 )
 
-type logHTTPHandler struct {
-	handler http.Handler
-}
-
-type loggedResponse struct {
-	http.ResponseWriter
-	status  int
-	content []byte
-}
-
-func (l *loggedResponse) WriteHeader(status int) {
-	l.status = status
-	l.ResponseWriter.WriteHeader(status)
-}
-
-func (l *loggedResponse) Write(content []byte) (int, error) {
-	l.content = content
-	return l.ResponseWriter.Write(content)
-}
-
-// ServeHTTP intercepts the default http.Handler implementation in order to
-// handle HTTP request and response logging. It provides a default response
-// containing a 200 OK status, and an empty byte array as the content, if not
-// specified in the Responsewriter.
-//
-// It logs the request status, method, and URL, and the response status and
-// content length.
-func (h *logHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	lw := &loggedResponse{ResponseWriter: w, status: 200, content: []byte{}}
-	h.handler.ServeHTTP(lw, r)
-	log.Printf("%s %s %s status:%d content-length:%d",
-		r.RemoteAddr, r.Method, r.URL, lw.status, len(lw.content))
-}
-
 func main() {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
@@ -63,10 +29,7 @@ func main() {
 
 	LoadConfig(config)
 
-	server := &http.Server{
-		Addr:    ":" + strconv.Itoa(GetPort()),
-		Handler: &logHTTPHandler{Route()},
-	}
+	server := NewServer(GetPort())
 
 	hup := make(chan os.Signal, 1)
 	signal.Notify(hup, syscall.SIGHUP)
---
+++ server.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+const timeLayout = "02/Jan/2006:15:04:05 -0700"
+
+// Return a new RB Gateway HTTP server.
+func NewServer(port int) http.Server {
+	addr := fmt.Sprintf(":%d", port)
+
+	return http.Server{
+		Addr:    addr,
+		Handler: logRequest{Route()},
+	}
+}
+
+// A specialized `http.ResponseWriter` for logging.
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	status     int
+	contentLen int
+}
+
+// Write the header for the given status code.
+func (l *loggingResponseWriter) WriteHeader(status int) {
+	l.status = status
+	l.ResponseWriter.WriteHeader(status)
+}
+
+// Write the given content to the client.
+func (l *loggingResponseWriter) Write(content []byte) (int, error) {
+	l.contentLen += len(content)
+	return l.ResponseWriter.Write(content)
+}
+
+// Wrap an `http.Handler` in a closure that will log the request in Common log Format.
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger := loggingResponseWriter{
+			ResponseWriter: w,
+			status:         200,
+			contentLen:     0,
+		}
+		handler.ServeHTTP(&logger, r)
+		log.Printf("%s - - [%s] \"%s %s %s\" %d %d",
+			r.RemoteAddr,
+			time.Now().Format(timeLayout),
+			r.Method,
+			r.URL,
+			r.Proto,
+			logger.status,
+			logger.contentLen)
+	})
+}
