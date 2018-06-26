cb4924c6646c58fc746e40f5e55ad3190d6b1e47
Barret Rennie <barret@beanbaginc.com>
2018-06-26 17:19:47 -0600

    Add a command-line interface to rb-gateway
    We now bring in `kingpin` for argument parsing. This allows us to
    specify the path to the configuration file.
    
    Testing Done:
    Ran `go fmt ./...`.
    Ran `go test ./...`.
    Ran `rb-gateway --config` with a different config file.
    
    Reviewed at https://reviews.reviewboard.org/r/10015/

---
+++ commands/serve.go
@@ -0,0 +1,92 @@
+package commands
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/reviewboard/rb-gateway/api"
+	"github.com/reviewboard/rb-gateway/config"
+)
+
+func Serve(configPath string) {
+	var cfg *config.Config
+	configWatcher := config.Watch(configPath)
+
+	select {
+	case cfg = <-configWatcher.NewConfig:
+		break
+
+	case err := <-configWatcher.Errors:
+		log.Fatal("Could not watch configuration file: ", err)
+	}
+
+	if cfg.TokenStorePath == ":memory:" {
+		log.Fatal("Cannot use memory store outside of tests.")
+	}
+
+	api, err := api.New(*cfg)
+	if err != nil {
+		log.Fatalf("Could not create API: %s", err.Error())
+	}
+
+	hup := make(chan os.Signal, 1)
+	signal.Notify(hup, syscall.SIGHUP)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	for {
+		var newCfg *config.Config = nil
+		shouldExit := false
+		log.Println("Starting rb-gateway server on port", cfg.Port)
+		log.Println("Quit the server with CONTROL-C.")
+
+		server := api.Serve()
+
+		select {
+		case newCfg = <-configWatcher.NewConfig:
+			log.Println("Detected configuration change, reloading...")
+
+		case err := <-configWatcher.Errors:
+			log.Fatal("Unexpected error: ", err)
+
+		case <-hup:
+			log.Println("Received SIGHUP, reloading configuration...")
+
+			var err error
+			if newCfg, err = configWatcher.ForceReload(); err != nil {
+				log.Fatal("Unexpected error: ", err)
+			}
+
+		case <-interrupt:
+			shouldExit = true
+			signal.Reset(os.Interrupt)
+			log.Println("Received SIGINT, shutting down...")
+			log.Println("CONTROL-C again to force quit.")
+		}
+
+		err = api.Shutdown(server)
+		if err != nil {
+			log.Fatalf("An error occurred while shutting down the server: %s", err.Error())
+		}
+
+		log.Println("Server shut down.")
+
+		if shouldExit {
+			break
+		}
+
+		if newCfg != nil {
+			if newCfg.TokenStorePath == ":memory:" {
+				log.Println("Failed to reload configuration: cannot use memory store outside of tests.")
+				log.Println("Configuration was not reloaded.")
+			} else if err = api.SetConfig(*newCfg); err != nil {
+				log.Printf("Failed to reload configuration: %s\n", err.Error())
+			} else {
+				log.Println("Configuration reloaded.")
+			}
+		}
+	}
+}
